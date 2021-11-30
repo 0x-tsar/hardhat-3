176d5ef8c269f777005e4c11857c47b06fc5ae14
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-30 07:52:34 -0300

    :sparkles:

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,5 +9,16 @@ contract Token {
 
     constructor() {
         balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address to, uint256 amount) external {
+        require(balances[msg.sender] >= amount, "not enough tokens");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
     }
 }
