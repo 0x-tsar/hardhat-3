6ec3884eb3402ff1ea10224e9a4095b4838a5179
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-30 09:20:35 -0300

    :sparkles:

--- test/Token.js
+++ test/Token.js
@@ -6,5 +6,16 @@ describe("Token contract", () => {
   beforeEach(async () => {
     Token = await ethers.getContractFactory("Token");
     token = await Token.deploy();
+    [owner, addr1, addr2, _] = await ethers.getSigners();
+  });
+
+  describe("Deployment", () => {
+    it("Should set the right owner", async () => {
+      expect(await token.owner()).to.equal(owner.address);
+    });
+
+    it("Should assigned total supply of tokens to the owner", async () => {
+      const ownerBalance = await token.balanceOf(owner.address);
+    });
   });
 });
